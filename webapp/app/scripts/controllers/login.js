da96f77e653ecbbaf4d7376f8d74ec868b7c04a5
Kyle Buchanan <kyle.buchanan@bcbsnc.com>
2013-03-27 17:25:57 -0400

    added function to open child browser

--- webapp/app/scripts/controllers/login.js
+++ webapp/app/scripts/controllers/login.js
@@ -5,6 +5,10 @@
 		document.addEventListener("deviceready", onDeviceReady, false);
 
 		function onDeviceReady() {
+			/*
+			 * if the user has made it to this screen and they hit the back button,
+			 * we'll need to take the user out of the application
+			 */
 			if (device.platform === "Android") {
 				document.addEventListener("backbutton", onBackKeyDown, false);
 			}
@@ -12,6 +16,22 @@
 
 		function onBackKeyDown() {
 			navigator.app.exitApp();
-		}
+		};
+
+		$scope.openChildBrowser = function(provider) {
+			var url = '';
+
+			switch (provider) {
+				case 'google':
+					url = 'http://www.google.com';
+				break;
+
+				case 'facebook':
+					url = 'http://www.facebook.com';
+				break;
+			}
+
+			var ref = window.open(url, '_blank', 'location=yes');
+		};
 	}]);
 })();
