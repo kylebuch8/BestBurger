f2577fbdf08002371cc83efd2521f649639cf5bd
Kyle Buchanan <kyle.buchanan@bcbsnc.com>
2013-03-28 08:50:54 -0400

    using single quotes now

--- webapp/app/scripts/app.js
+++ webapp/app/scripts/app.js
@@ -1,29 +1,29 @@
 (function(window) {
-    "use strict";
+    'use strict';
 
     window.Best = {
-        appName : "BestBurger",
-        tokenString : "token"
+        appName : 'BestBurger',
+        tokenString : 'token'
     };
 
     angular.module(Best.appName, [])
-        .config(["$routeProvider", function($routeProvider) {
+        .config(['$routeProvider', function($routeProvider) {
             $routeProvider
-                .when("/", {
-                    templateUrl : "views/main.html",
-                    controller : "MainCtrl"
+                .when('/', {
+                    templateUrl : 'views/main.html',
+                    controller : 'MainCtrl'
                 })
-                .when("/login", {
-                  templateUrl: "views/login.html",
-                  controller: "LoginCtrl"
+                .when('/login', {
+                  templateUrl: 'views/login.html',
+                  controller: 'LoginCtrl'
                 })
                 .otherwise({
-                    redirectTo : "/"
+                    redirectTo : '/'
                 });
         }])
-        .run(["$location", function($location) {
+        .run(['$location', function($location) {
             if (!localStorage.getItem(Best.tokenString)) {
-                $location.path("/login");
+                $location.path('/login');
             }
         }]);
 })(window);
