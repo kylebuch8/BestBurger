4d3f855c11ad095749c146b3fe11162f924433d0
Kyle Buchanan <kyle.buchanan@bcbsnc.com>
2013-03-28 08:51:16 -0400

    using single quotes now

--- webapp/app/scripts/controllers/main.js
+++ webapp/app/scripts/controllers/main.js
@@ -1,11 +1,11 @@
 (function() {
-	"use strict";
+	'use strict';
 
-	angular.module(Best.appName).controller("MainCtrl", ["$scope", function($scope) {
+	angular.module(Best.appName).controller('MainCtrl', ['$scope', function($scope) {
 		$scope.awesomeThings = [
-			"HTML5 Boilerplate",
-			"AngularJS",
-			"Karma"
+			'HTML5 Boilerplate',
+			'AngularJS',
+			'Karma'
 		];
 	}]);
 })();
