208e3e23a5419e4091493a8bfbd7fdb93284db56
Kyle Buchanan <kyle.buchanan@bcbsnc.com>
2013-03-28 23:33:06 -0400

    worked on google and facebook login

--- webapp/app/scripts/controllers/login.js
+++ webapp/app/scripts/controllers/login.js
@@ -1,7 +1,7 @@
 (function() {
 	'use strict';
 
-	angular.module(Best.appName).controller('LoginCtrl', ['$scope', function($scope) {
+	angular.module(Best.appName).controller('LoginCtrl', ['$scope', '$location', function($scope, $location) {
 		document.addEventListener('deviceready', onDeviceReady, false);
 
 		function onDeviceReady() {
@@ -23,17 +23,36 @@
 
 			switch (provider) {
 				case 'google':
-					url = 'http://www.google.com';
+					url = 'https://accounts.google.com/o/oauth2/auth?response_type=code&scope=https://www.googleapis.com/auth/userinfo.profile&client_id=124967647113.apps.googleusercontent.com&redirect_uri=http://google.com/';
 				break;
 
 				case 'facebook':
-					url = 'http://www.facebook.com';
+					url = 'https://www.facebook.com/dialog/oauth?client_id=212457378769328&redirect_uri=http://www.google.com/&scope=email&response_type=token';
 				break;
 			}
 
-			var ref = window.open(url, '_blank', 'location=no');
+			var ref = window.open(url, '_blank', 'location=yes');
 			ref.addEventListener('loadstart', function(event) {
-				console.log(event.url);
+				var patt = (provider === 'google') ? new RegExp(/http:\/\/www.google.com\/[^;]+code=([^"]+)/) : new RegExp(/http:\/\/www.google.com\/[^;]+access_token=([^"]+)/),
+    				result = patt.exec(event.url);
+
+    			console.log('Loading url: ' + event.url);
+
+    			if (result === null) {
+    				console.log('no code');
+    			} else {
+    				console.log('we have a code');
+    				console.log(result[1]);
+    				alert(result[1]);
+    				ref.close();
+
+    				console.log("send to profile page");
+    				document.removeEventListener('backbutton', onBackKeyDown, false);
+
+    				$scope.$apply(function() {
+    					$location.path('/profile');
+    				});
+    			}
 			});
 		};
 	}]);
