3231118e4ec1be683375950d05f938490fbbecdf
Kyle Buchanan <kyle.buchanan@bcbsnc.com>
2013-03-31 12:29:52 -0400

    exit the application if you're on the home screen

--- webapp/app/scripts/controllers/main.js
+++ webapp/app/scripts/controllers/main.js
@@ -7,5 +7,21 @@
 			'AngularJS',
 			'Karma'
 		];
+
+		document.addEventListener('deviceready', onDeviceReady, false);
+
+		function onDeviceReady() {
+			/*
+			 * if the user has made it to this screen and they hit the back button,
+			 * we'll need to take the user out of the application
+			 */
+			if (device.platform === 'Android') {
+				document.addEventListener('backbutton', onBackKeyDown, false);
+			}
+		};
+
+		function onBackKeyDown() {
+			navigator.app.exitApp();
+		};
 	}]);
 })();
