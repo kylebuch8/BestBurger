0ec1a04ddd6543136d0e1f3656d2c2f57f1c7436
Kyle Buchanan <kyle.buchanan@bcbsnc.com>
2013-03-31 12:28:48 -0400

    get user info from google or facebook

--- webapp/app/scripts/controllers/profile.js
+++ webapp/app/scripts/controllers/profile.js
@@ -1,7 +1,51 @@
 (function() {
 	'use strict';
 
-	angular.module(Best.appName).controller('ProfileCtrl', ['$scope', function($scope) {
-		
+	angular.module(Best.appName).controller('ProfileCtrl', ['$scope', '$http', function($scope, $http) {
+		/*
+		 * theoretically the user should have a authProvider and token in local storage.
+		 * using the authProvider, we can tell which endpoint to hit to get basic
+		 * profile information about the user
+		 *
+		 * if the authProvider is google, we can use the following endpoint to retrieve info
+		 * https://www.googleapis.com/oauth2/v1/userinfo?access_token={accessToken}
+		 */
+
+		if (localStorage.getItem(Best.authProvider) === 'google') {
+			console.log('authProvider is google');
+			console.log('token is: ' + localStorage.getItem(Best.tokenString));
+
+			var token = localStorage.getItem(Best.tokenString),
+				url = 'https://www.googleapis.com/oauth2/v1/userinfo?access_token=' + token;
+
+			console.log('url is: ' + url);
+
+			$http
+				.get(url)
+				.success(function(data) {
+					$scope.name = data.name;
+					$scope.picture = data.picture;
+				})
+				.error(function() {
+					alert('there was a problem');
+				});
+		}
+
+		if (localStorage.getItem(Best.authProvider) === 'facebook') {
+			var token = localStorage.getItem(Best.tokenString),
+				url = 'https://graph.facebook.com/me?fields=picture,id,name,first_name,last_name,email&access_token=' + token;
+
+			console.log('url is: ' + url);
+
+			$http
+				.get(url)
+				.success(function(data) {
+					$scope.name = data.name;
+					$scope.picture = data.picture.data.url;
+				})
+				.error(function() {
+					alert('there was a problem');
+				});
+		}
 	}]);
 })();
