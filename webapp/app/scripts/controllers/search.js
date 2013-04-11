a7e04be9a00abf37363d571f3dafc3915010938c
Kyle Buchanan <kyle.buchanan@bcbsnc.com>
2013-04-10 22:07:10 -0400

    using deferreds in foursquare service

--- webapp/app/scripts/controllers/search.js
+++ webapp/app/scripts/controllers/search.js
@@ -5,17 +5,11 @@
 		// remove back button listener if it's there
 		document.removeEventListener('backbutton', $rootScope.exitApp, false);
 
-	    $scope.FoursquareSvc = FoursquareSvc;
-
-	    $scope.$watch('venue', function(newVenue) {
+	    $scope.$watch('venue', function(newVenue, oldValue, scope) {
 			if(typeof(newVenue) !== 'undefined' && newVenue !== '') {
-				FoursquareSvc.suggestVenue(newVenue);
-			}
-		});
-
-		$scope.$watch('FoursquareSvc.suggest', function(newVenues) {
-			if(typeof(newVenues) !== 'undefined') {
-				$scope.venues = newVenues;
+				FoursquareSvc.suggestVenue(newVenue).success(function(data) {
+					scope.venues = data;
+				});
 			}
 		});
 	  }]);
