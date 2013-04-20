fc5ab430b0c83130fe9222d5c19a801fab6ce202
Kyle Buchanan <kyle.buchanan@bcbsnc.com>
2013-04-20 08:39:59 -0400

    created a fixed navbar

--- webapp/app/scripts/services/ratingssvc.js
+++ webapp/app/scripts/services/ratingssvc.js
@@ -38,6 +38,188 @@
                         name: 'Bull McCabe\s'
                     },
                     createDate: 'April 15th, 2013'
+                },
+                {
+                    id: 3,
+                    user: {
+                        name: 'Luke Dary'
+                    },
+                    rating: 4,
+                    burger: {
+                        name: 'The Meat Monster'
+                    },
+                    venue: {
+                        name: 'Bull McCabe\s'
+                    },
+                    createDate: 'April 15th, 2013'
+                },
+                {
+                    id: 4,
+                    user: {
+                        name: 'Luke Dary'
+                    },
+                    rating: 4,
+                    burger: {
+                        name: 'The Meat Monster'
+                    },
+                    venue: {
+                        name: 'Bull McCabe\s'
+                    },
+                    createDate: 'April 15th, 2013'
+                },
+                {
+                    id: 2,
+                    user: {
+                        name: 'Luke Dary'
+                    },
+                    rating: 4,
+                    burger: {
+                        name: 'The Meat Monster'
+                    },
+                    venue: {
+                        name: 'Bull McCabe\s'
+                    },
+                    createDate: 'April 15th, 2013'
+                },
+                {
+                    id: 1,
+                    user: {
+                        name: 'Kyle Buchanan'
+                    },
+                    rating: 5,
+                    burger: {
+                        name: 'The Heartattack'
+                    },
+                    venue: {
+                        name: 'City Beverage'
+                    },
+                    createDate: 'April 16th, 2013'
+                },
+                {
+                    id: 2,
+                    user: {
+                        name: 'Luke Dary'
+                    },
+                    rating: 4,
+                    burger: {
+                        name: 'The Meat Monster'
+                    },
+                    venue: {
+                        name: 'Bull McCabe\s'
+                    },
+                    createDate: 'April 15th, 2013'
+                },
+                {
+                    id: 3,
+                    user: {
+                        name: 'Luke Dary'
+                    },
+                    rating: 4,
+                    burger: {
+                        name: 'The Meat Monster'
+                    },
+                    venue: {
+                        name: 'Bull McCabe\s'
+                    },
+                    createDate: 'April 15th, 2013'
+                },
+                {
+                    id: 4,
+                    user: {
+                        name: 'Luke Dary'
+                    },
+                    rating: 4,
+                    burger: {
+                        name: 'The Meat Monster'
+                    },
+                    venue: {
+                        name: 'Bull McCabe\s'
+                    },
+                    createDate: 'April 15th, 2013'
+                },
+                {
+                    id: 2,
+                    user: {
+                        name: 'Luke Dary'
+                    },
+                    rating: 4,
+                    burger: {
+                        name: 'The Meat Monster'
+                    },
+                    venue: {
+                        name: 'Bull McCabe\s'
+                    },
+                    createDate: 'April 15th, 2013'
+                },
+                {
+                    id: 1,
+                    user: {
+                        name: 'Kyle Buchanan'
+                    },
+                    rating: 5,
+                    burger: {
+                        name: 'The Heartattack'
+                    },
+                    venue: {
+                        name: 'City Beverage'
+                    },
+                    createDate: 'April 16th, 2013'
+                },
+                {
+                    id: 2,
+                    user: {
+                        name: 'Luke Dary'
+                    },
+                    rating: 4,
+                    burger: {
+                        name: 'The Meat Monster'
+                    },
+                    venue: {
+                        name: 'Bull McCabe\s'
+                    },
+                    createDate: 'April 15th, 2013'
+                },
+                {
+                    id: 3,
+                    user: {
+                        name: 'Luke Dary'
+                    },
+                    rating: 4,
+                    burger: {
+                        name: 'The Meat Monster'
+                    },
+                    venue: {
+                        name: 'Bull McCabe\s'
+                    },
+                    createDate: 'April 15th, 2013'
+                },
+                {
+                    id: 4,
+                    user: {
+                        name: 'Luke Dary'
+                    },
+                    rating: 4,
+                    burger: {
+                        name: 'The Meat Monster'
+                    },
+                    venue: {
+                        name: 'Bull McCabe\s'
+                    },
+                    createDate: 'April 15th, 2013'
+                },
+                {
+                    id: 2,
+                    user: {
+                        name: 'Luke Dary'
+                    },
+                    rating: 4,
+                    burger: {
+                        name: 'The Meat Monster'
+                    },
+                    venue: {
+                        name: 'Bull McCabe\s'
+                    },
+                    createDate: 'April 15th, 2013'
                 }
             ];
 
