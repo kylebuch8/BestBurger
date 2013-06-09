fa8b6075a81a75acf8f6de0817123111d8b35cc2
Kyle Buchanan <kyle.buchanan@bcbsnc.com>
2013-06-09 14:59:40 -0400

    foursquare library and config

---
+++ backend/application/config/foursquare.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['clientID'] = 'YJO3HB1UU0XLEYPQGE4LJH4MANMFRMEZDUBKTAWXDH0A5V1F';
+$config['clientSecret'] = 'Z3Z114WIR4LXAYEUMV5KLZLYCXNUKDZ2WOOZDOWCBEG2YXIV';
+$config['redirectUri'] = 'http://localhost/CodeIgniter-Foursquare/step2';
+
+$config['token_session'] = 'foursquare';
+$config['baseUrl'] = 'https://api.foursquare.com/v2/';
+$config['authUrl'] = 'https://foursquare.com/oauth2/authenticate';
+$config['tokenUrl'] = 'https://foursquare.com/oauth2/access_token';
