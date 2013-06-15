b3ba92ec145d11056b057244e44dd5baa2f31bea
Kyle Buchanan <kyle.buchanan@bcbsnc.com>
2013-06-15 11:47:38 -0400

    beginning to work on storing user credentials

--- backend/application/controllers/users.php
+++ backend/application/controllers/users.php
@@ -10,6 +10,29 @@ class Users extends MY_Controller {
     	$this->load->view('login');
     }
 
+    public function storeCredentials() {
+    	$return = array();
+
+    	/*
+    	 * we are receiving a request payload and we need to sanitize it
+    	 */
+    	$data = json_decode(file_get_contents('php://input'), true);
+    	$data = $this->security->xss_clean($data);
+
+    	/*
+    	 * we need to make sure that we receive an authProvider and a
+    	 * token from the post
+    	 */
+    	if (!array_key_exists('authProvider', $data) || !array_key_exists('token', $data)) {
+    		$return['status'] = 'error';
+    		$return['message'] = 'Both authProvider and token are required';
+    	} else {
+    		$return['status'] = 'success';
+    	}
+
+    	echo json_encode($return);
+    }
+
     private function _generatetoken() {
 		$token = "";
 		
